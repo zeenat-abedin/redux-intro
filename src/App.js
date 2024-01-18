d59c194371bc34964ca4e2a3c387340ccb5dc095
zeenat-abedin <zeenu.23@gmail.com>
2024-01-18 22:01:46 +0530

    Added a reducer to manage the state of a bank account

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import "./index.css";
 
 /*
@@ -24,7 +25,30 @@ const initialState = {
   isActive: false,
 };
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "openAccount":
+      return { ...state, balance: 500, isActive: true };
+    case "deposit":
+      return "";
+    case "withdraw":
+      return "";
+    case "requestLoan":
+      return "";
+    case "payLoan":
+      return "";
+    case "closeAccount":
+      return "";
+    default:
+      throw new Error("Unknown");
+  }
+}
+
 export default function App() {
+  const [{ balance, loan, isActive }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   return (
     <div className="App">
       <h1>useReducer Bank Account</h1>
@@ -32,32 +56,32 @@ export default function App() {
       <p>Loan: X</p>
 
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={isActive}>
           Open account
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Deposit 150
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Withdraw 50
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Request a loan of 5000
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Pay loan
         </button>
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => {}} disabled={!isActive}>
           Close account
         </button>
       </p>
