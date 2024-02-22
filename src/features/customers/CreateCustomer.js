e87fa6dbd08cdec2a6e6c345eaebdd4f58dd5b6f
zeenat <zeenu.23@gmail.com>
2024-02-22 23:39:41 +0530

    Update the `createCustomer` action to use positional arguments instead of an object.

--- src/features/customers/CreateCustomer.js
+++ src/features/customers/CreateCustomer.js
@@ -10,7 +10,7 @@ function CreateCustomer() {
 
   function handleClick() {
     if (!fullName || !nationalId) return;
-   dispatch(createCustomer({  fullName,  nationalId}))
+   dispatch(createCustomer( fullName, nationalId))
   }
 
   return (
