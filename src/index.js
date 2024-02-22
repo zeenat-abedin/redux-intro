40e18b3c0995c50ac6885330f3640e1c6872a303
zeenat <zeenu.23@gmail.com>
2024-02-22 22:27:35 +0530

    remove space

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from  './store';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider  store={store}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
