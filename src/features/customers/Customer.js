ad5f927281624841423a564a03e8ced3e9e1805e
zeenat <zeenu.23@gmail.com>
2024-02-22 23:03:56 +0530

    Log customer object to console

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
+  const customer = useSelector((store) => store.customer);
+  console.log('customer',customer)
   return <h2>👋 Welcome, %NAME%</h2>;
 }
 
