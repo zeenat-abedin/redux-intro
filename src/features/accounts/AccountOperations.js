b5a7ffd0bf7baa58a9a2944b4d86ebe85b0738cf
zeenat <zeenu.23@gmail.com>
2024-02-23 22:35:01 +0530

    Update `deposit` function to include `currency` parameter

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -15,7 +15,7 @@ function AccountOperations() {
 
   function handleDeposit() {
     if (!depositAmount) return;
-    dispatch(deposit(depositAmount))
+    dispatch(deposit(depositAmount, currency))
     setDepositAmount("")
   }
 
