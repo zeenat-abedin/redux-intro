75e98337e16eca6369557a1e768064470a7ded21
zeenat <zeenu.23@gmail.com>
2024-02-23 16:25:26 +0530

    Changed the application state management from using a local reducer to using Redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useSelector } from "react-redux";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
 
 import "./index.css";
@@ -6,55 +6,20 @@ import AccountOperations from "./features/accounts/AccountOperations";
 import Customer from "./features/customers/Customer";
 import CreateCustomer from "./features/customers/CreateCustomer";
 
-const initialState = {
-  balance: 0,
-  loan: 0,
-  isActive: false,
-};
-
-function reducer(state, action) {
-  if (!state.isActive && action.type === !"openAccount") return state;
-
-  switch (action.type) {
-    case "openAccount":
-      return { ...state, balance: 500, isActive: true };
-    case "deposit":
-      return { ...state, balance: state.balance + action.payload };
-    case "withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload,
-        balance: state.balance + action.payload,
-      };
-    case "payLoan":
-      return {
-        ...state,
-        loan: 0,
-        balance: state.balance - state.loan,
-      };
-    case "closeAccount":
-      if (state.loan > 0 || state.balance !== 0) return state;
-      return initialState;
-    default:
-      throw new Error("Unknown");
-  }
-}
 
  function App() {
-  const [{ balance, loan, isActive }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+ const fullName = useSelector((state) => state.customer.fullName)
   return (
     <div>
       <h1> The React-Redux Bank</h1>
-      <CreateCustomer/>
-      <Customer/>
+      {
+        fullName === " "? (<CreateCustomer />) : (<>
+           <Customer/>
       <AccountOperations/>
       <BalanceDisplay/>
+        </>)
+      }
+     
     </div>
   );
 }
