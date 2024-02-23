6fc5b8589323e71b7ddc74363a4f9eb2d3ddf52f
zeenat <zeenu.23@gmail.com>
2024-02-23 23:20:42 +0530

    Add currency conversion for deposits not in USD.

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -25,11 +25,25 @@ export default function accountReducer(state = initialStateAccount, action) {
   }
 }
 
-export function deposit(amount) {
-  return {
-    type: "account/deposit",
-    payload: amount,
-  };
+export function deposit(amount, currency) {
+  if (currency === 'USD') 
+    return {
+      type: "account/deposit",
+      payload: amount,
+    };      
+  return async function (dispatch, getState) {
+    //API Call
+    const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`)
+    const data = await res.json()
+    console.log(data)
+    const converted = data.rates.USD
+
+    //return action
+    return {
+      type: "account/deposit",
+      payload: converted
+    }
+  }
 }
 
 export function withdraw(amount) {
