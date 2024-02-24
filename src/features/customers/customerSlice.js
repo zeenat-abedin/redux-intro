a7045217fdc0d9dbd2582e7926db8c0a2f4d835b
zeenat <zeenu.23@gmail.com>
2024-02-24 11:12:07 +0530

    Update customer slice action type from 'account/updateName' to 'customer/updateName'

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -31,7 +31,7 @@ export function createCustomer(fullName, nationalID) {
 
 export function updateName(fullName) {
   return {
-    type: 'account/updateName',
+    type: 'customer/updateName',
     payload: fullName
   }
 }
