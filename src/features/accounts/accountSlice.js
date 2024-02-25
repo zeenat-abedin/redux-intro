ba8dbc242ef1b05ffccfa8722df24ef9ad3fb146
zeenat <zeenu.23@gmail.com>
2024-02-25 17:00:44 +0530

    Update isLoading state after deposit

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -13,6 +13,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance += action.payload;
+      state.isLoading = false
     },
     withdraw(state, action) {
       state.balance -= action.payload;
